44b630844e167d0e6220f4dd6221b9550e39b9ed
Ilya <ilayburtsev@yandex.ru>
2024-09-21 19:06:09 +0300

    Исправление в mask для PEP 8

--- src/mask.py
+++ src/mask.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: str) -> str:
     """ "Функцию маскировки номера банковской карты в виде XXXX XX** **** XXXX"""
 
-    return  f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(card_number: str) -> str:
