c90d3d868ea088c8c6764c749ae46c2acca19687
Ilya <ilayburtsev@yandex.ru>
2024-09-24 09:57:03 +0300

    Добавление функции для сортировки списка операций по их дате в модуль processing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.mask import get_mask_account, get_mask_card_number
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 # 7000 7922 8960 6361
@@ -24,5 +24,30 @@ print(
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ]
+    )
+)
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+# {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        True,
     )
 )
--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,8 @@
 def filter_by_state(process: list, state: str = "EXECUTED") -> list:
-    """ "Фильтрует список операций по статусу выполнения"""
+    """ "Функция для фильтрации списка операций по статусу выполнения"""
     return list(filter(lambda proces: proces["state"] == state, process))
+
+
+def sort_by_date(process: list, order_reverse: bool = False) -> list:
+    """ " Функция для сортировки списка операций по дате исполнения"""
+    return sorted(process, key=lambda proces: proces["date"], reverse=order_reverse)
