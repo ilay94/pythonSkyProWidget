00cff31c7912292a9668e00b53d11a9a5cc1bd15
Ilya <ilayburtsev@yandex.ru>
2024-09-24 15:53:30 +0300

    Исправление значение по умолчанию order_reverse на True в функции sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -3,6 +3,6 @@ def filter_by_state(process: list, state: str = "EXECUTED") -> list:
     return list(filter(lambda proces: proces["state"] == state, process))
 
 
-def sort_by_date(process: list, order_reverse: bool = False) -> list:
+def sort_by_date(process: list, order_reverse: bool = True) -> list:
     """ " Функция для сортировки списка операций по дате исполнения"""
     return sorted(process, key=lambda proces: proces["date"], reverse=order_reverse)
