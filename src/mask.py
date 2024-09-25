04c93891fe0476d03296d8d236722f0ea856e2a0
Ilya <ilayburtsev@yandex.ru>
2024-09-25 20:49:15 +0300

    Добавление тестов для модуля

--- src/mask.py
+++ src/mask.py
@@ -1,9 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """ "Функцию маскировки номера банковской карты в виде XXXX XX** **** XXXX"""
-
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    if type(card_number) is str and card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return "Некорректный номер карты"
 
 
 def get_mask_account(card_number: str) -> str:
     """ "Функцию маскировки номера банковского счета в виде **XXXX"""
-    return f"**{card_number[-4:]}"
+    if type(card_number) is str and card_number.isdigit() and len(card_number) == 20:
+        return f"**{card_number[-4:]}"
+    else:
+        return "Некорректный номер счета"
---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def unmask_card_number():
+    return "7000792289606361"
+
+
+@pytest.fixture
+def unmask_account():
+    return "73654108430135874305"
